eebe5f070d5bfa5612dace8378583032a98399f8
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-06-23 14:04:43 -0700

    New files created

---
+++ 102-counting_sort.c
@@ -0,0 +1,50 @@
+#include "sort.h"
+#include <stdlib.h>
+
+void counting_sort(int *array, size_t size)
+{
+	int *sorted, *count, i, max_val;
+	size_t j;
+
+	sorted = malloc(sizeof(int) * size);
+
+	max_val = array[0];
+	for (j = 1; j < size; j++)
+	{
+		if (array[j] > max_val)
+		{
+			max_val = array[j];
+		}
+	}
+
+	count = malloc(sizeof(int) * (max_val + 1));
+
+	for (i = 0; i <= max_val; i++)
+	{
+		count[i] = 0;
+	}
+
+	for (j = 0; j < size; j++)
+	{
+		count[array[j]]++;
+	}
+
+	for (i = 1; i <= max_val; i++)
+	{
+		count[i] += count[i - 1];
+	}
+	print_array(count, max_val + 1);
+
+	for (i = size - 1; i >= 0; i--)
+	{
+		sorted[--count[array[i]]] = array[i];
+	}
+
+	for (j = 0; j < size; j++)
+	{
+		array[j] = sorted[j];
+	}
+
+	free(count);
+	free(sorted);
+}
