207b0565de7137f03c895f0e76a1325ddb2b84bb
injitanze <injijoshua@gmail.com>
2023-06-23 10:00:35 -0700

    3

--- sort.h
+++ sort.h
@@ -1,7 +1,7 @@
 #ifndef SORT_H
 #define SORT_H
 #include <stddef.h>
-
+#include <stdlib.h>
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -23,7 +23,6 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 listint_t *swap_node(listint_t *node, listint_t **list);
-int partition(int *array, int low, int high, size_t size);
 void quicksort(int *array, int low, int high, size_t size);
 
 
